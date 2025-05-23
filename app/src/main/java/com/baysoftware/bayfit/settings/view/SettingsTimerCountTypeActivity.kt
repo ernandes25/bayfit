e5f1de89074458854290f1c60023389c07a6de88
ernandes25 <antonioernandes25@gmail.com>
2025-05-23 15:46:37 -0300

    implementação parcial, opção de treino Livre ou Tempo

--- app/src/main/java/com/baysoftware/bayfit/settings/view/SettingsTimerCountTypeActivity.kt
+++ app/src/main/java/com/baysoftware/bayfit/settings/view/SettingsTimerCountTypeActivity.kt
@@ -3,60 +3,83 @@ package com.baysoftware.bayfit.settings.view
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.baysoftware.bayfit.databinding.ActivitySettingsTimerCountTypeBinding
+import com.baysoftware.bayfit.history.view.HistoryListActivity
+import com.baysoftware.bayfit.home.composable.ActivitySettingsTimerCountTypeScreen
+import com.baysoftware.bayfit.home.composable.SettingsScreen
 import com.baysoftware.bayfit.preferences.UserManager
+import com.baysoftware.bayfit.running.view.RunningTimerActivity
 import kotlinx.coroutines.launch
 
 class SettingsTimerCountTypeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySettingsTimerCountTypeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySettingsTimerCountTypeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setupListeners()
-    }
-
-    private fun setupListeners() {
-        lifecycleScope.launch {
-            val restType =
-                UserManager.getInstance().readTimerMode(this@SettingsTimerCountTypeActivity)
-
-            if (restType == UserManager.TimerMode.FREE) {
-                binding.radioButton1.isChecked = true
-            } else if (restType == UserManager.TimerMode.PREDEFINED) {
-                binding.radioButton2.isChecked = true
-            }
-        }
-
-        binding.buttonOkcountType.setOnClickListener {
-            lifecycleScope.launch {
-
-                val selectedButton = if (binding.radioButton1.isChecked) {
-                    finish()
-                    UserManager.TimerMode.FREE
-                } else if (binding.radioButton2.isChecked) {
-                    val intent = Intent(
-                        this@SettingsTimerCountTypeActivity,
-                        SettingsTimerSetterActivity::class.java
-                    )
+        setContent {
+            ActivitySettingsTimerCountTypeScreen(
+                onRadioButton1Select = {
+                    val intent = Intent(this, SettingsTimerCountTypeActivity::class.java)
                     startActivity(intent)
-                    finish() // Finaliza a atividade atual
-                    UserManager.TimerMode.PREDEFINED
-                } else {
-                    Toast.makeText(
-                        this@SettingsTimerCountTypeActivity,
-                        "Por favor, selecione uma opção",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    UserManager.TimerMode.UNDEFINED
+                },
+                onRadioButton2Select = {
+                    val intent = Intent(this, SettingsTimerCountTypeActivity::class.java)
+                    startActivity(intent)
+                },
+                onButtonOk = {
+                    val intent = Intent(this, SettingsTimerCountTypeActivity::class.java)
+                    startActivity(intent)
                 }
-                UserManager.getInstance()
-                    .saveTimerMode(this@SettingsTimerCountTypeActivity, selectedButton)
-            }
+            )
         }
+
+
+
     }
 }
+
+
+
+//    private fun setupListeners() {
+//        lifecycleScope.launch {
+//            val restType =
+//                UserManager.getInstance().readTimerMode(this@SettingsTimerCountTypeActivity)
+//
+//            if (restType == UserManager.TimerMode.FREE) {
+//                binding.radioButton1.isChecked = true
+//            } else if (restType == UserManager.TimerMode.PREDEFINED) {
+//                binding.radioButton2.isChecked = true
+//            }
+//        }
+//
+//        binding.buttonOkcountType.setOnClickListener {
+//            lifecycleScope.launch {
+//
+//                val selectedButton = if (binding.radioButton1.isChecked) {
+//                    finish()
+//                    UserManager.TimerMode.FREE
+//                } else if (binding.radioButton2.isChecked) {
+//                    val intent = Intent(
+//                        this@SettingsTimerCountTypeActivity,
+//                        SettingsTimerSetterActivity::class.java
+//                    )
+//                    startActivity(intent)
+//                    finish() // Finaliza a atividade atual
+//                    UserManager.TimerMode.PREDEFINED
+//                } else {
+//                    Toast.makeText(
+//                        this@SettingsTimerCountTypeActivity,
+//                        "Por favor, selecione uma opção",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                    UserManager.TimerMode.UNDEFINED
+//                }
+//                UserManager.getInstance()
+//                    .saveTimerMode(this@SettingsTimerCountTypeActivity, selectedButton)
+//            }
+//        }
+//    }
+//}
