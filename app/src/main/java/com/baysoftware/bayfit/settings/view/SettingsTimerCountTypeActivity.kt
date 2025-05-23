5bc1c6f6a34e10cb8922855d37e10406a53d6628
ernandes25 <antonioernandes25@gmail.com>
2025-05-23 16:45:00 -0300

    implementação parcial, opção de treino Livre ou Tempo

--- app/src/main/java/com/baysoftware/bayfit/settings/view/SettingsTimerCountTypeActivity.kt
+++ app/src/main/java/com/baysoftware/bayfit/settings/view/SettingsTimerCountTypeActivity.kt
@@ -12,14 +12,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.lifecycleScope
 import com.baysoftware.bayfit.home.composable.TimerTypeOption
-import com.baysoftware.bayfit.databinding.ActivitySettingsTimerCountTypeBinding
-import com.baysoftware.bayfit.history.view.HistoryListActivity
 import com.baysoftware.bayfit.home.composable.ActivitySettingsTimerCountTypeScreen
-import com.baysoftware.bayfit.home.composable.SettingsScreen
 import com.baysoftware.bayfit.preferences.UserManager
-import com.baysoftware.bayfit.running.view.RunningTimerActivity
 import kotlinx.coroutines.launch
-
 
 
 class SettingsTimerCountTypeActivity : AppCompatActivity() {
